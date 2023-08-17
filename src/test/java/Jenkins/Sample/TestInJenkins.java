4c943581300b8dcfb5055635c1a9788d10d9da52
Suhas <sghodake@innominds.com>
2023-08-17 18:38:43 +0530

    New Project

---
+++ src/test/java/Jenkins/Sample/TestInJenkins.java
@@ -0,0 +1,19 @@
+package Jenkins.Sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestInJenkins {
+
+	@Test
+	public void testJenkins() {
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.google.com");
+	}
+	
+	
+}
