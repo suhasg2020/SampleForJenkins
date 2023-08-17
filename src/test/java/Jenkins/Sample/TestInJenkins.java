cc898cdf17564c7ad47b2dd5a4a24026e739f2c2
Suhas <sghodake@innominds.com>
2023-08-17 22:20:04 +0530

    Driver done

--- src/test/java/Jenkins/Sample/TestInJenkins.java
+++ src/test/java/Jenkins/Sample/TestInJenkins.java
@@ -15,7 +15,7 @@ public class TestInJenkins {
 	public void testJenkins() {
 		System.out.println("I am trying:" + "--user-data-dir=~/.config/google-chrome");
 		//WebDriverManager.chromedriver().setup();
-		System.setProperty("webdriver.chrome.driver" ,"/usr/bin/chromedriver");
+		System.setProperty("webdriver.chrome.driver" ,"./driver/chromedriver");
 		// WebDriver driver = new ChromeDriver();
 		// driver.get("https://www.google.com");
 		ChromeOptions chrome_options = new ChromeOptions();
