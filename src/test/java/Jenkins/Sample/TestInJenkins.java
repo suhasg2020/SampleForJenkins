061ef534f61e4f60217b08332be567b037fe67c7
Suhas <sghodake@innominds.com>
2023-08-17 19:58:59 +0530

    New setup

--- src/test/java/Jenkins/Sample/TestInJenkins.java
+++ src/test/java/Jenkins/Sample/TestInJenkins.java
@@ -2,6 +2,7 @@ package Jenkins.Sample;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import com.github.dockerjava.api.model.Driver;
@@ -12,10 +13,25 @@ public class TestInJenkins {
 
 	@Test
 	public void testJenkins() {
-	//WebDriverManager.chromedriver().setup();
-    System.setProperty("webdriver.chrome.driver" , System.getProperty("user.dir")+"/driver/chromedriver");
-	WebDriver driver = new ChromeDriver();
-	driver.get("https://www.google.com");
+	WebDriverManager.chromedriver().setup();
+    //System.setProperty("webdriver.chrome.driver" , System.getProperty("user.dir")+"/driver/chromedriver");
+	//WebDriver driver = new ChromeDriver();
+	//driver.get("https://www.google.com");
+	ChromeOptions chrome_options = new ChromeOptions();
+
+			chrome_options.addArguments("--no-sandbox");
+			//chrome_options.addArguments("--user-data-dir=~/.config/google-chrome")
+			WebDriver driver = new ChromeDriver(chrome_options);
+
+			String url = "https://www.google.com";
+
+			driver.get(url) ;
+
+			String get_url = driver.getCurrentUrl();
+
+			System.out.println(get_url);
+	
+	
 	}
 	
 	
