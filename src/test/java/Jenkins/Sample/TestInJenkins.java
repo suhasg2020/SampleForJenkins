9fda1c33094d2ccc669ad2d4a4eda592bd5be6e5
Suhas <sghodake@innominds.com>
2023-08-17 19:36:45 +0530

    Added chrome driver

--- src/test/java/Jenkins/Sample/TestInJenkins.java
+++ src/test/java/Jenkins/Sample/TestInJenkins.java
@@ -4,13 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import com.github.dockerjava.api.model.Driver;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestInJenkins {
 
 	@Test
 	public void testJenkins() {
-	WebDriverManager.chromedriver().setup();
+	//WebDriverManager.chromedriver().setup();
+    System.setProperty("webdriver.chrome.driver" , System.getProperty("user.dir")+"/driver/chromedriver");
 	WebDriver driver = new ChromeDriver();
 	driver.get("https://www.google.com");
 	}
